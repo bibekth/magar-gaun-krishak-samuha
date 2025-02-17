39b3ace6da21fb6b35bd2f154e0398f2819a1f59
Bibek Thapa Magar <bibek.thapa0521@gmail.com>
2025-02-17 22:13:27 +0545

    login was get, changed to post method

--- routes/api.php
+++ routes/api.php
@@ -4,8 +4,8 @@ use App\Http\Controllers\APIController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:api');
-Route::get('/login',[APIController::class,'login'])->name('login');
+// Route::get('/user', function (Request $request) {
+//     return $request->user();
+// })->middleware('auth:api');
+Route::post('/login',[APIController::class,'login'])->name('login');
 Route::get('/main',[APIController::class,'main'])->name('main');
