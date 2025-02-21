128eb36fdd72715f3b2e5f890a971c2d48688196
Bibek Thapa Magar <bibek.thapa0521@gmail.com>
2025-02-21 10:25:26 +0545

    made member_id as primary in users and removed id as primary

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -12,7 +12,7 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('users', function (Blueprint $table) {
-            $table->id();
+            $table->id()->primary(false);
             $table->unsignedBigInteger('member_id')->unique()->index()->nullable()->primary(true);
             $table->string('name');
             $table->string('phone_number')->unique()->nullable();
