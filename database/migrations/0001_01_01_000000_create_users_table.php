8800090c101a4a6bfc1c45809ee4766beda2f925
Bibek Thapa Magar <bibek.thapa0521@gmail.com>
2025-02-21 10:23:48 +0545

    made member_id as primary in users

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -13,7 +13,7 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->unsignedBigInteger('member_id')->unique()->index()->nullable();
+            $table->unsignedBigInteger('member_id')->unique()->index()->nullable()->primary(true);
             $table->string('name');
             $table->string('phone_number')->unique()->nullable();
             $table->string('email')->unique()->nullable();
