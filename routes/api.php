421561e3eb846464cf942b822a04a0e9052bb466
Bibek Thapa Magar <bibek.thapa0521@gmail.com>
2025-06-03 11:57:51 +0545

    webhook testing

--- routes/api.php
+++ routes/api.php
@@ -4,9 +4,6 @@ use App\Http\Controllers\APIController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-// Route::get('/user', function (Request $request) {
-//     return $request->user();
-// })->middleware('auth:api');
 Route::post('/login',[APIController::class,'login'])->name('login');
 Route::middleware('token.api')->group(function (){
     Route::get('/main',[APIController::class,'main'])->name('main');
