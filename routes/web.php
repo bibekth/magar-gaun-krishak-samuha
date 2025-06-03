308cdaebf2215181b483404e711c6061979ab9d8
Bibek Thapa Magar <bibek.thapa0521@gmail.com>
2025-06-03 11:46:00 +0545

    webhook testing

--- routes/web.php
+++ routes/web.php
@@ -18,16 +18,19 @@ Route::post('/github/webhooks', function () {
         $payload = file_get_contents("php://input");
         $signature = $_SERVER["HTTP_X_HUB_SIGNATURE_256"] ?? "";
         $hash = "sha256=" . hash_hmac("sha256", $payload, $secret);
+
         if (!hash_equals($hash, $signature)) {
             http_response_code(403);
             exit("Invalid Signature");
         }
 
         $data = json_decode($payload, true);
+
         if ($data["ref"] === "refs/heads/new") {
             exec("cd ~/public_html/magar-gaun-krishak-samuha && git pull origin main 2>&1", $output, $returnCode);
             file_put_contents("webhook.log", implode('\n', $output), FILE_APPEND);
         }
+        
         return response()->json('success', 200);
     } catch (Exception $e) {
         return response()->json($e->getMessage(), 500);
